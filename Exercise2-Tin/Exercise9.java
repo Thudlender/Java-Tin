432d00e0ddd132f4ae13302534df34946b158d25
Thudlender <tthunder1688@gmail.com>
2023-02-10 10:45:48 +0700

    I'm feel sleepy

--- Exercise2-Tin/Exercise9.java
+++ Exercise2-Tin/Exercise9.java
@@ -1,13 +1,19 @@
+import java.text.DecimalFormat;
 import java.util.Scanner;
 
 public class Exercise9 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.print("Enter money(Dollar) : ");
-        int dollar = input.nextInt();
-        double bath = 35.50;
-        double money = dollar * bath;
-        System.out.print("You have money  " + money + " Bath");
+        System.out.print("Enter money(THB) : ");
+        double thb = input.nextDouble();
+        double rate = 35.50;
+        double usd = thb / rate;
+        input.close();
+        String pattern = "0.00";
+        DecimalFormat df = new DecimalFormat(pattern);
+        String usd2 = df.format(usd);
+
+        System.out.print(thb + " THB to USD is " + usd2 + " USD");
 
     }
 }
